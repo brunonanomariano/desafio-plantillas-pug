f5f0cfde0ae246173c14d8482009c770854ad24e
Mariano Bruno <mariano.-ezequiel@hotmail.com>
2022-11-22 21:29:10 -0300

    Entrega desafio plantillas pug

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ const app = express()
 const productsRouter = require('./routes/products')
 
 const server = app.listen(8080, () => console.log('Server UP'))
+app.use(express.urlencoded({extended: true}))
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+let prod = []
+
+app.get('/', (req, resp)=>{
+    resp.render('formulario')
+})
+
+app.get('/productos', (req, resp)=>{
+    resp.render('productos', {p: prod})
+})
+
+app.post('/productos', (req, resp)=>{
+    prod.push(req.body)
+    resp.redirect('/')
+})
 
 app.use(express.json())
 app.use(express.static('public'))
